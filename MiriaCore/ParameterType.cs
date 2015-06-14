a1545ad47f7496e26a7bda2cf5500a410c34269d
yuinore <yuinore@hotmail.co.jp>
2015-06-15 03:54:08 +0900

    Add ParameterType.cs

---
+++ MiriaCore/ParameterType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiriaCore
+{
+    abstract class ParameterType
+    {
+        public abstract double MaxValue { get; }
+        public abstract double MinValue { get; }
+        public abstract double DefaultValue { get; }
+    }
+}
