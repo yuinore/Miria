d6742365905a2fbb4e1be89e703e79ef896a3d3f
yuinore <yuinore@hotmail.co.jp>
2015-06-17 10:32:04 +0900

    Add FadeOut.cs

---
+++ MiriaCore/AudioProcesses/FadeOut.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiriaCore.AudioProcesses
+{
+    class FadeOut : AudioProcess
+    {
+        int samplingRate = 44100;
+
+        public double FadeTime { get; set; }  // seconds
+
+        public FadeOut()
+        {
+            FadeTime = 1.0;
+        }
+
+        public override float[][] Do(float[][] buffer)
+        {
+            int samplesFade = (int)Math.Round(FadeTime * samplingRate);
+
+            for (int ch = 0; ch < buffer.Length; ch++)
+            {
+                int start = Math.Max(0, buffer[ch].Length - samplesFade);
+
+                for (int i = start; i < buffer[ch].Length; i++)
+                {
+                    buffer[ch][i] *= 1.0f - (i - (buffer[ch].Length - samplesFade)) / (float)samplesFade;
+                }
+            }
+
+            return buffer;
+        }
+    }
+}
