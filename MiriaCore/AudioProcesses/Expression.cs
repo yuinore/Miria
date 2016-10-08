535f2e7e679d2b7d643b028d5aefdec88c7ec8f5
yuinore <yuinore@hotmail.co.jp>
2016-10-08 22:32:57 +0900

    ステレオファイルに対して正しくExpressionを処理するよう修正

--- MiriaCore/AudioProcesses/Expression.cs
+++ MiriaCore/AudioProcesses/Expression.cs
@@ -17,8 +17,9 @@ namespace MiriaCore.AudioProcesses
         public Expression()
         {
             ExpressionCode = "x => Math.max(Math.log(x * x) * 0.1 / Math.log(10) + 1.0, 0.0)";  // 0dB～-100dBを1.0～0.0にマップ
+            //ExpressionCode = "x => (x >= Math.pow(10, -70.0 * 0.05) ? 1 : 0)";  // 与えられたdB以上の音だけ抜き出し
         }
-        
+
         public override float[][] Do(float[][] buffer)
         {
             // TODO: ステレオの両方のチャンネルに対して処理する
@@ -26,44 +27,51 @@ namespace MiriaCore.AudioProcesses
             using (var engine = new V8ScriptEngine())
             {
                 int BLOCK_SIZE = 8192;
-
-                object aaa = engine.Evaluate("[1,2,3]");
-
+                
                 engine.Execute("__mapper = (" + ExpressionCode + ")");
 
-                List<float> wholeResult = new List<float>();
+                var code1 = engine.Compile(
+                    "(function(){" +
+                    "var __result = new Array(__inputArray.length); " +
+                    "for(__j=0;__j<__inputArray.length;__j++){__result[__j]=__mapper(__inputArray[__j]);} " +
+                    "return __result.join(',');})()"
+                    );
 
-                var code1 = engine.Compile("result = new Array(inputArray.length); for(j=0;j<inputArray.length;j++){result[j]=__mapper(inputArray[j]);} result.join(',')");
-
-                for (int blockId = 0; blockId < (buffer[0].Length - 1) / BLOCK_SIZE + 1; blockId++)
+                return buffer.Select(channelOfBuffer =>
                 {
-                    int fromInclusive = BLOCK_SIZE * blockId;
-                    int toExclusive = Math.Min(BLOCK_SIZE * (blockId + 1), buffer[0].Length);
+                    List<float> wholeResult = new List<float>();
 
-                    var s = new StringBuilder();
-                    s.Append("inputArray = [");
-                    s.Append(string.Join(",", Enumerable.Range(fromInclusive, toExclusive - fromInclusive).Select(i => buffer[0][i] + "")));  // こんなアホみたいな方法でも意外と速くて草
-                    s.Append("];");
-
-                    engine.Execute(engine.Compile(s.ToString()));
-
-                    dynamic result = engine.Evaluate(code1);
-
-                    // 配列を渡すのではなく、文字列に変換して渡すことにより大幅に高速化
-
-                    float[] result3 = ((string)result).Split(',').Select(x =>
+                    for (int blockId = 0; blockId < (channelOfBuffer.Length - 1) / BLOCK_SIZE + 1; blockId++)
                     {
-                        if (x == "Infinity") return float.PositiveInfinity;
-                        if (x == "-Infinity") return float.NegativeInfinity;
-                        if (x == "NaN") return float.NaN;
+                        int fromInclusive = BLOCK_SIZE * blockId;
+                        int toExclusive = Math.Min(BLOCK_SIZE * (blockId + 1), channelOfBuffer.Length);
 
-                        return float.Parse(x);
-                    }).ToArray();
+                        var s = new StringBuilder();
+                        s.Append("__inputArray = [");
+                        s.Append(string.Join(",", Enumerable.Range(fromInclusive, toExclusive - fromInclusive).Select(i => channelOfBuffer[i] + "")));  // こんなアホみたいな方法でも意外と速くて草
+                        s.Append("];");
 
-                    wholeResult.AddRange(result3);
-                }
+                        engine.Execute(engine.Compile(s.ToString()));
 
-                return new float[][] { wholeResult.ToArray(), wholeResult.ToArray() };  // 冷静に考えると前半と後半が同じ参照だとダメだな
+                        dynamic result = engine.Evaluate(code1);
+
+                        // 配列を渡すのではなく、文字列に変換して渡すことにより大幅に高速化
+
+                        float[] result3 = ((string)result).Split(',').Select(x =>
+                        {
+                            if (x == "Infinity") return float.PositiveInfinity;
+                            if (x == "-Infinity") return float.NegativeInfinity;
+                            if (x == "NaN") return float.NaN;
+                            // NaNやInfinityは残さない方が良い気もします・・・
+
+                            return float.Parse(x);
+                        }).ToArray();
+
+                        wholeResult.AddRange(result3);
+                    }
+
+                    return wholeResult.ToArray();
+                }).ToArray();
             }
         }
     }
